9d22663c7800c3a6c41090109dcae897d26e4548
pepperpapa <zhongxin2506@outlook.com>
2017-11-04 11:30:18 +0800

    创建管理员帐户, /admin页面可以登录了

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
+# 表tags（标签）：
+# * id
+# * name --标签名称
+class Tags(models.Model):
+    # TIP: Tags定义要在Notes之前,否则包Tags未定义
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
+# 表notes（文章）：
+# * id
+# * title --标题
+# * pub_date --时间日期
+# * content  --文章内容
+# * tag_id --标签
+# * like_num --点赞数
+class Notes(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("发表时间")
+    content = models.CharField(max_length=50000)
+    tag_id = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    like_num = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
